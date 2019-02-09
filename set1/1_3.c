3e2043c3080c0f39ff074b62d8a58315c4ccf86f
chdre <christerdr@outlook.com>
2019-02-09 11:58:24 +0100

    Finished set2

--- set1/1_3.c
+++ set1/1_3.c
@@ -2,37 +2,43 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-        int nx = 5;//atoi(argv[1]);
-        int ny = 5;//atoi(argv[2]);
-        int nz = 5;//atoi(argv[3]);
+	int nx = atoi(argv[1]);
+	int ny = atoi(argv[2]);
+	int nz = atoi(argv[3]);
 
+	double ***arr = malloc(nx*sizeof(double **));
 
-        double ***arr = malloc(nx*sizeof(double ***));
-        //double ***arr = (double ***) malloc (sizeof(double ***)*ny);
+	for(int i = 0; i < nx; i++) {
+		arr[i] = malloc(ny*sizeof(double *));
+		for(int j = 0; j < ny; j++) {
+			arr[i][j] = malloc(nz*sizeof(double));
+		}
+	}
 
-        for(int i = 0; i < nx; i++) {
-                arr[i] = (double **) malloc(ny*sizeof(double *));
-                for(int j = 0; j < ny; j++) {
-                        arr[i][j] = (double *) malloc(nz*sizeof(double));
-                }
-        }
+	for(int i = 0; i < nx; i++) {
+		for(int j = 0; j < ny; j++) {
+			for(int k = 0; k < nz; k++) {
+				arr[i][j][k] = (double) i*ny*nz + j*nz + k;
+			}
+		}
+	}
 
-        for(int i = 0; i < nx; i++) {
-                for(int j = 0; j < ny; j++) {
-                        for(int k = 0; k < nz; k++) {
-                                arr[i][j][k] = (double) i*ny*nz + j*nz + k;
-                        }
-                }
-        }
+	for(int i = 0; i < nx; i++) {
+		for(int j = 0; j < ny; j++) {
+			for(int k = 0; k < nz; k++) {
+				printf("%f\n", arr[i][j][k]);
+			}
+		}
+	}
 
-        for(int i = 0; i < nx; i++) {
-                for(int j = 0; j < ny; j++) {
-                        for(int k = 0; k < nz; k++) {
-                                printf("%f", arr[i][j][k]);
-                        }
-                }
-        }
-        free(arr);
+	// deallocate 3D array
+	for(int i = 0; i < nx; i++) {
+		for(int j = 0; j < ny; j++) {
+			free(arr[i][j]);
+		}
+		free(arr[i]);
+	}
+	free(arr);
 
-        return 0;
+	return 0;
 }
